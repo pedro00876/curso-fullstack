5202612ae7643cb8b4f621de417e4cda876c5915
pedro00876 <pcchagas00@gmail.com>
2025-10-29 16:30:35 -0300

    Variavel let

--- 2_JAVASCRIPT_BASICO/script.js
+++ 2_JAVASCRIPT_BASICO/script.js
@@ -4,3 +4,31 @@ var user = 'Pedro'
 
 // Substituindo o valor de var
 user = 'Leticia'
+
+//////////////////////////////////////////////////////////////////////////////
+
+// 2 - Case Sensitive
+
+/*
+  Quando  uma linguagem de programação é cas-seneitive significa
+  que ela é sensível a letras maiúsculas e minúsculas.
+
+  Por exemplo: "Rodrigo" é considerado deiferente de "rodrigo".
+*/
+
+var product = 'Teclado mecânico'
+console.log(product)
+
+var Product = 'Mouse gamer'
+console.log(Product)
+
+//////////////////////////////////////////////////////////////////////////////
+
+// 1 - let
+// Declarando variável
+
+let user = 'Pedrinho'
+
+// Substituindo o valor de let
+
+user = 'Pedro'
